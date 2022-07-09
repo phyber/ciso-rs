d5d9409f1b79c2694a38a1ad2917128e8e221076
David O'Rourke <david.orourke@gmail.com>
2022-07-09 18:26:38 +0100

    Comment on BlockIndex

--- src/block_index.rs
+++ src/block_index.rs
@@ -11,18 +11,22 @@ use crate::consts::CISO_HEADER_SIZE;
 pub struct BlockIndex(Vec<u32>);
 
 impl BlockIndex {
+    // Create a new BlockIndex with the given size.
     pub fn new(num_blocks: usize) -> Self {
         Self(vec![0u32; num_blocks])
     }
 
+    // Get the block index at the given offset
     pub fn get(&self, offset: usize) -> u32 {
         self.0[offset]
     }
 
+    // Get an iter_mut for the inner vec
     pub fn iter_mut(&mut self) -> ::std::slice::IterMut<u32> {
         self.0.iter_mut()
     }
 
+    // Read the block index from the given file
     pub fn read_from(&mut self, file: &mut File) -> Result<&mut Self> {
         let mut buffer: [u8; 4] = [0; 4];
         let total_blocks = self.0.len();
@@ -36,10 +40,13 @@ impl BlockIndex {
         Ok(self)
     }
 
+    // Set the block index at offset to content
     pub fn set(&mut self, offset: usize, content: u32) {
         self.0[offset] = content;
     }
 
+    // Write the block index out to the given file.
+    // Seeks to after the file header before writing.
     pub fn write_to(&mut self, file: &mut File) -> Result<()> {
         // Seek to after the header, which is where the block index lives.
         file.seek(SeekFrom::Start(CISO_HEADER_SIZE as u64))?;
