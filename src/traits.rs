e953830e2f43660b6e441d6368626a683ffec376
David O'Rourke <david.orourke@gmail.com>
2022-07-09 18:31:47 +0100

    Comment on traits

--- src/traits.rs
+++ src/traits.rs
@@ -12,6 +12,7 @@ pub trait ReadSizeAt {
 }
 
 impl ReadSizeAt for File {
+    // Read size bytes from the file at the given offset
     fn read_size_at(&mut self, size: u64, offset: u64) -> Result<Vec<u8>, Error> {
         // Seek to the correct location in the file
         self.seek(SeekFrom::Start(offset))?;
@@ -22,6 +23,7 @@ impl ReadSizeAt for File {
         Ok(data)
     }
 
+    // Read size bytes from the file at its current stream position
     fn read_size(&mut self, size: u64) -> Result<Vec<u8>, Error> {
         // Attempt to read
         let mut data = vec![0u8; size as usize];
