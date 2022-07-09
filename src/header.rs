998fc1ba53a23362e76ef6d8f6ea1f6637f6569e
David O'Rourke <david.orourke@gmail.com>
2022-07-09 18:30:47 +0100

    Comment on CisoHeader

--- src/header.rs
+++ src/header.rs
@@ -31,6 +31,7 @@ pub struct CisoHeader {
 }
 
 impl CisoHeader {
+    // Get a new CisoHeader with total_bytes set
     pub fn new_with_total_bytes(total_bytes: u64) -> Self {
         Self {
             total_bytes,
@@ -42,19 +43,23 @@ impl CisoHeader {
         }
     }
 
+    // Get the alignment from the header
     pub fn align(&self) -> u8 {
         self.align
     }
 
+    // Get the block size from the header
     pub fn block_size(&self) -> u32 {
         self.block_size
     }
 
+    // Derive the total blocks from information in the header
     pub fn total_blocks(&self) -> usize {
         (self.total_bytes / self.block_size as u64) as usize
     }
 
     // There might be a safe way of doing this, if so, replace this.
+    // Get the header as a byte array, in preparation for writing it to disk.
     pub unsafe fn as_bytes(&self) -> &[u8] {
         ::std::slice::from_raw_parts(
             (self as *const Self) as *const u8,
@@ -62,6 +67,7 @@ impl CisoHeader {
         )
     }
 
+    // Write the header out to the given file
     pub fn to_file(&self, file: &mut File) -> Result<(), ::std::io::Error> {
         let data = unsafe { self.as_bytes() };
         file.write_all(data)
@@ -71,6 +77,7 @@ impl CisoHeader {
 impl TryFrom<&mut File> for CisoHeader {
     type Error = CisoError;
 
+    // Try and get a CisoHeader from the given file
     fn try_from(file: &mut File) -> Result<Self, CisoError> {
         let mut buffer: [u8; CISO_HEADER_SIZE as usize] = [0; CISO_HEADER_SIZE as usize];
 
@@ -80,11 +87,13 @@ impl TryFrom<&mut File> for CisoHeader {
         // Quick header check
         let magic: [u8; 4] = [buffer[0], buffer[1], buffer[2], buffer[3]];
 
+        // Ensure we have the correct file magic
         if u32::from_le_bytes(magic) != CISO_MAGIC {
             eprintln!("invalid file magic");
             return Err(CisoError::MagicError);
         }
 
+        // Get a new CisoHeader
         let header = CisoHeader {
             magic,
             header_size: u32::from_le_bytes(buffer[4..8].try_into().unwrap()),
@@ -95,6 +104,7 @@ impl TryFrom<&mut File> for CisoHeader {
             _reserved:   [buffer[22], buffer[23]],
         };
 
+        // Ensure that the block size and total bytes make sense
         if header.block_size == 0 || header.total_bytes == 0 {
             eprintln!(
                 "invalid block_size ({}) or total_bytes ({})",
@@ -104,6 +114,8 @@ impl TryFrom<&mut File> for CisoHeader {
             return Err(CisoError::HeaderError);
         }
 
+        // Some CISO software doesn't write the header properly, report this
+        // but ignore it.
         if header.header_size != CISO_HEADER_SIZE {
             eprintln!("Incorrect header size found, ignoring.");
         }
